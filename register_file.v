57e4c4950dd2ea46752484c61191608db50db2e6
Ben Oommen <benjamin.oommen1@gmail.com>
2023-04-15 11:28:17 -0500

    fix fmt

--- register_file.v
+++ register_file.v
@@ -1,9 +1,10 @@
-module register_file(input clk, 
+module register_file
+(input clk, 
     // READ PORT
-    input [3:0]instr_buffer_read_addr [7:0], output [20:0]instr_buffer_read_data [7:0],
+input [3:0] instr_buffer_read_addr[0:7], input instru_buffer_read_enable[0:7], output [20:0] instr_buffer_read_data[0:7],
 
     // WRITE PORT
-    input [20:0]retirement_write_data [2:0]
+input [19:0] retirement_write_data[0:2], input retirement_write_data_enable[0:2]
 );
 
     reg [15:0] values[15:0];
@@ -20,13 +21,13 @@ module register_file(input clk,
         integer j;
 
         for (i = 0; i < 8; i++) begin
-            m_instr_buffer_read_data[i] <= {values[instr_buffer_read_addr[i]], busy[instr_buffer_read_addr[i]], owner[instr_buffer_read_addr[i]]};
+            if (instru_buffer_read_enable[i])
+                m_instr_buffer_read_data[i] <= {values[instr_buffer_read_addr[i]], busy[instr_buffer_read_addr[i]], owner[instr_buffer_read_addr[i]]};
         end
 
         for (j = 0; j < 3; j++) begin
-            if (retirement_write_data[j][0]) begin
-                values[retirement_write_data[j][4:1]] <= retirement_write_data[j][20:5];
-            end 
+            if (retirement_write_data_enable[j])
+                values[retirement_write_data[j][3:0]] <= retirement_write_data[j][19:4];
         end
     end
 
