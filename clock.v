ffddbd4e542527794ad699310801883b705be628
Andrew McAlinden <andrew04@cs.utexas.edu>
2023-04-16 16:06:34 -0500

    made clock module

---
+++ clock.v
@@ -0,0 +1,11 @@
+`timescale 1ps/1ps
+module clock(output clk);
+
+    reg theClock = 1;
+    assign clk = theClock;
+    
+    always begin
+        #500;
+        theClock = !theClock;
+    end
+endmodule
