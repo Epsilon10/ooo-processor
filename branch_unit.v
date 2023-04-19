f43b3c81549053417bffc391e45c70ef82562667
Andrew McAlinden <andrew04@cs.utexas.edu>
2023-04-19 18:27:02 -0500

    Delay functional units by 1 cycle

--- branch_unit.v
+++ branch_unit.v
@@ -1,9 +1,15 @@
 `timescale 1ps/1ps
 
 module BranchUnit
-(input [3:0] opcode, input [3:0] in_index,
+(input [3:0] opcode, input [3:0] in_index, input in_valid,
 input [3:0] vt, input [3:0] va, input [3:0] vb, input [8:0] i, // we receive decoded values
 output out_valid, output [3:0] rob_index, output [15:0] branch_target, output branch_taken);
+
+    reg m_valid;
+
+    always @(posedge clk) begin 
+        m_valid <= in_valid;
+    end
 
     assign rob_index = in_index;
     assign branch_target = vt;
@@ -13,7 +19,7 @@ output out_valid, output [3:0] rob_index, output [15:0] branch_target, output br
     wire isJgt = opcode == 4'b1010;
     wire isJlt = opcode == 4'b1011;
 
-    assign out_valid = isJz || isJnz || isJgt || isJlt;
+    assign out_valid = m_valid;
 
     assign branch_taken = 
     isJz ? va == 0 : 
--- fxu.v
+++ fxu.v
@@ -1,19 +1,25 @@
 `timescale 1ps/1ps
 
 module FXU
-(input [3:0] opcode, input [3:0] in_index,
+(input clk,
+input [3:0] opcode, input [3:0] in_index, in_valid,
 input [3:0] vt, input [3:0] va, input [3:0] vb, input [8:0] i, // we receive decoded values
 output out_valid, output [3:0] rob_index, output [15:0] return_value);
 
+    reg m_valid;
+
+    always @(posedge clk) begin 
+        m_valid <= in_valid;
+    end
+
     assign rob_index = in_index;
+    assign out_valid = m_valid;
 
     wire isAdd = opcode == 4'b0000;
     wire isSub = opcode == 4'b0001;
     wire isMov = opcode == 4'b0100;
     wire isMovl = opcode == 4'b0101;
     wire isMovh = opcode == 4'b0110;
-
-    assign out_valid = isAdd || isSub || isMov || isMovl || isMovh;
 
     assign return_value = 
     isAdd ? va + vb : 
