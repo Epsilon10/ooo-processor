df5213c5ee8f6625caf00cc5a7e9e62197a643fa
Andrew McAlinden <andrew04@cs.utexas.edu>
2023-04-23 21:02:25 -0500

    made l1 cache

---
+++ l1cache.v
@@ -0,0 +1,24 @@
+`timescale 1ps/1ps
+
+module L1Cache
+(input clk,
+input [15:0] read_addr,
+output [15:0] data_out);
+
+    reg [15:0] data[0:16'hffff];
+
+    initial begin
+        $readmemh("mem.hex", data);
+    end
+
+    reg [15:0] m_data;
+    reg [15:0] m_read_addr;
+
+    assign data_out = m_data;
+
+    always @(posedge clk) begin
+        m_read_addr <= read_addr;
+        m_data <= data[m_read_addr];
+    end
+
+endmodule
